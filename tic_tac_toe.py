c1f80244ea4ebb42962641d919d44fb9401052e7
Robert <robert.straubmueller@gmail.com>
2025-12-05 08:31:20 +0100

    Fertiges Tic-Tac-Toe Code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,23 +1,107 @@
-# In this script you can write your code.
-# Start by writing all the functions.
-# In the last part after if __name__ == "__main__": you can call the functions to play your game.
-# If you run `python tic_tac_toe.py` in the command line the game will start. Try it out! ;)
+class board: 
 
-# Function for ... (displaying the board?)
-def blabla():
-    pass
+    def __init__(self):
+        self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+        self.player='O'
 
 
-# Function for... (choosing a player?)
-def blablabla():
-    pass
+    def eingabe(self):
+
+        # changing the Player
+        if self.player == 'x':
+            self.player = 'O'
+        else:
+            self.player = 'x'
 
 
-# ... write as many functions as you need
+        # input the row and colum
+        while True:
+            try:
+                row = int(input('gib eine Zeile ein (0-2): '))
+                if 0 <= row <= 2:
+                    break  # gültig, Schleife verlassen
+                else:
+                    print('Ungültig, nur 0, 1 oder 2 erlaubt!')
+            except ValueError:
+                print('Falsche Eingabe, bitte eine Zahl eingeben.')
+
+        while True:
+            try:
+                colum = int(input('gib eine Spalte ein (0-2): '))
+                if 0 <= colum <= 2:
+                    break
+                else:
+                    print('Ungültig, nur 0, 1 oder 2 erlaubt!')
+            except ValueError:
+                print('Falsche Eingabe, bitte eine Zahl eingeben.')  
+
+        # print the input
+        print(f'Player {self.player} wählt row = {row} & colum = {colum}')
 
 
-# Tic-tac-toe game
-if __name__ == "__main__":
+        # Feld schon besetzt
+        if self.board[row][colum] != ' ':
+            print("\nWenn wir Freunde wären würdest du so'n Scheiss überhaupt nicht machen")
+            return self.eingabe()  # Spieler erneut wählen lassen
 
-    # Start a new round of Tic-tac-toe
-    print("Welcome to a new round of Tic-Tac-Toe!")
+        # gültiger Zug
+        self.board[row][colum] = self.player
+        for i, line in enumerate(self.board):
+            print(' | '.join(line))
+            if i < 2:
+                print('---------')  # Trenner zwischen den Reihen
+
+
+        # Sieg prüfen
+        if self.check():
+            return True
+
+        # Unentschieden prüfen
+        if self.is_full():
+            print("Unentschieden! Keine freien Felder mehr.")
+            return True
+
+        return False
+
+
+    def check(self):
+        # check rows
+        if all(cell == self.player for cell in self.board[0]):
+            return self.winning()
+        elif all(cell == self.player for cell in self.board[1]):
+            return self.winning()
+        elif all(cell == self.player for cell in self.board[2]):
+            return self.winning()
+
+        # check colum
+        elif self.board[0][0] == self.board[1][0] == self.board[2][0] == self.player:
+            return self.winning()
+        elif self.board[0][1] == self.board[1][1] == self.board[2][1] == self.player:
+            return self.winning()
+        elif self.board[0][2] == self.board[1][2] == self.board[2][2] == self.player:
+            return self.winning()
+
+        # check diagonal
+        elif self.board[0][0] == self.board[1][1] == self.board[2][2] == self.player:
+            return self.winning()
+        elif self.board[0][2] == self.board[1][1] == self.board[2][0] == self.player:
+            return self.winning()
+        else:   
+            return False
+
+    
+    def winning(self):
+        print(f'Player {self.player} winns!!')
+        return True
+    
+    def is_full(self):
+        return all(cell != ' ' for row in self.board for cell in row)
+
+
+
+        
+board = board()
+print("Let' Play Tic-Tac-Toe")
+while True:
+   if board.eingabe():
+       break
